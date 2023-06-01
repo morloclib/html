a44f28e766c56ad26cc0df7018052c534b1cc021
Zebulun Arendsee <zbwrnz@gmail.com>
2023-05-31 19:48:58 -0500

    Add read and write functions

--- html.py
+++ html.py
@@ -6,15 +6,17 @@ def toHtml(x) -> str:
         return x.to_html()
     elif(isinstance(x, str)):
         return html.escape(x)
-    elif(isinstance(x, list)):
-        return "<ol>" + "".join(["<li>" + toHtml(item) + "</li>" for item in x]) + "</ol>"
-    elif(isinstance(x, set)):
-        return "<ol>" + "".join(["<li>" + toHtml(item) + "</li>" for item in x]) + "</ol>"
     elif(isinstance(x, dict)):
         return "<ol>" + "".join(["<li>" + toHtml(key) + " : " + toHtml(val) + "</li>" for (key,val) in x.items()]) + "</ol>"
+    elif(isinstance(x, (list, tuple, set))):
+        return "<ol>" + "".join(["<li>" + toHtml(item) + "</li>" for item in x]) + "</ol>"
     else:
         return html.escape(str(x))
 
-def writeHtml(html, filename):
+def tag(tag_str, x):
+    return "<" + tag_str + ">" + x + "</" + tag_str + ">"
+
+
+def writeHtml(filename, html):
     with open(filename, "w") as f:
         f.write(html)
